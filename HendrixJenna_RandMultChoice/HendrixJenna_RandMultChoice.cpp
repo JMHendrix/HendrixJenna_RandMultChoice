292c3e8bc6ab8fdac079053997af6410e2676669
Jenna Hendrix <JMHendrix@student.fullsail.edu>
2022-06-17 15:59:44 -0400

    Finished Random Multiple Choice console app.

--- HendrixJenna_RandMultChoice/HendrixJenna_RandMultChoice.cpp
+++ HendrixJenna_RandMultChoice/HendrixJenna_RandMultChoice.cpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 #include<vector>
+#include <string>
 
+bool ValidateAnswer(char answer, int questionInt);
 int main()
 {
+	bool goAgain = true;
 	std::cout << "Question" << std::endl;
 	
 	//vector to hold the questions
@@ -24,7 +27,7 @@ int main()
 	std::string question3 = "What is my favorite color?";
 	questions.push_back(question3);
 
-	std::string question4 = "What is the national mammal of the United States?";
+	std::string question4 = "What is the national MAMMAL of the United States?";
 	questions.push_back(question4);
 
 	std::string question5 = "What series is not an HBO original?";
@@ -32,64 +35,149 @@ int main()
 
 	//create a vector for the answers to each of the questions.
 	std::vector<std::string> question1Answers;
-	question1Answers.push_back("David Gilmour");
-	question1Answers.push_back("Jimi Hendrix");
-	question1Answers.push_back("Synyster Gates");
-	question1Answers.push_back("Eddie Van Halen");
+	question1Answers.push_back("A. David Gilmour");
+	question1Answers.push_back("B. Jimi Hendrix");
+	question1Answers.push_back("C. Synyster Gates");
+	question1Answers.push_back("D. Eddie Van Halen");
 
 	answers.push_back(question1Answers);
 
 	std::vector<std::string> question2Answers;
-	question2Answers.push_back("Sheep");
-	question2Answers.push_back("Panda");
-	question2Answers.push_back("Snake");
-	question2Answers.push_back("Chicken");
+	question2Answers.push_back("A. Sheep");
+	question2Answers.push_back("B. Panda");
+	question2Answers.push_back("C. Snake");
+	question2Answers.push_back("D. Chicken");
 
 	answers.push_back(question2Answers);
 
 	std::vector<std::string> question3Answers;
-	question3Answers.push_back("Red");
-	question3Answers.push_back("Black");
-	question3Answers.push_back("Green");
-	question3Answers.push_back("Purple");
+	question3Answers.push_back("A. Red");
+	question3Answers.push_back("B. Black");
+	question3Answers.push_back("C. Green");
+	question3Answers.push_back("D. Purple");
 
 	answers.push_back(question3Answers);
 
 	std::vector<std::string> question4Answers;
-	question4Answers.push_back("Bald Eagle");
-	question4Answers.push_back("Grizzly Bear");
-	question4Answers.push_back("Lion");
-	question4Answers.push_back("Bald Eagle");
+	question4Answers.push_back("A. Bald Eagle");
+	question4Answers.push_back("B. Grizzly Bear");
+	question4Answers.push_back("C. Lion");
+	question4Answers.push_back("D. American Bison");
 
 	answers.push_back(question4Answers);
 
 	std::vector<std::string> question5Answers;
-	question5Answers.push_back("VEEP");
-	question5Answers.push_back("Stranger Things");
-	question5Answers.push_back("Game of Thrones");
-	question5Answers.push_back("Big Little Lies");
+	question5Answers.push_back("A. Stranger Things");
+	question5Answers.push_back("B. VEEP");
+	question5Answers.push_back("C. Game of Thrones");
+	question5Answers.push_back("D. Big Little Lies");
 
 	answers.push_back(question5Answers);
 
 	//Display random question, ask for answer, tell user if they are correct.
-	srand(time(0));
-	int randInt;
-	randInt = rand() % 5;
-	std::cout << questions[randInt] << std::endl;
+	while (goAgain) {
+		srand(time(0));
+		int randInt;
+		randInt = rand() % 5;
+		std::cout << questions[randInt] << std::endl;
 
-	for (int i = 0; i < answers[randInt].size(); i++) {
-		std::cout << answers[randInt][i] << std::endl;
+		for (int i = 0; i < answers[randInt].size(); i++) {
+			std::cout << answers[randInt][i] << std::endl;
+		}
+
+		std::cout << "\nEnter your answer here: ";
+		char userAnswer;
+		std::cin >> userAnswer;
+
+		goAgain = ValidateAnswer(userAnswer, randInt);
 	}
-
-	std::cout << "\nEnter your answer here: ";
-	std::string userAnswer;
-	std::cin >> userAnswer;
-	std::cout << userAnswer;
 	
-
 }
 
+bool ValidateAnswer(char answer, int questionInt) {
 
+	answer = toupper(answer);
+	
+	switch (questionInt) {
+	case 0:
+		//question 1
+		if (answer == 'B') {
+			//user was correct
+			std::cout << "\nGreat Job!";
+
+		}
+		else {
+			//incorrect answer
+			std::cout << "\nThat's incorrect.";
+
+		}
+		break;
+	case 1:
+		//question 2
+		if (answer == 'C') {
+			//user was correct
+			std::cout << "\nGreat Job!";
+
+		}
+		else {
+			//incorrect answer
+			std::cout << "\nThat's incorrect.";
+
+		}
+		break;
+	case 2:
+		//question 3
+		if (answer == 'D') {
+			//user was correct
+			std::cout << "\nGreat Job!";
+
+		}
+		else {
+			//incorrect answer
+			std::cout << "\nThat's incorrect.";
+
+		}		
+		break;
+	case 3:
+		//question 4
+		if (answer == 'D') {
+			//user was correct
+			std::cout << "\nGreat Job!";
+
+		}
+		else {
+			//incorrect answer
+			std::cout << "\nThat's incorrect.";
+
+		}
+		break;
+	case 4: 
+		//question 5
+		if (answer == 'A') {
+			//user was correct
+			std::cout << "\nGreat Job!";
+
+		}
+		else {
+			//incorrect answer
+			std::cout << "\nThat's incorrect.";
+
+		}
+		break;
+	}
+
+	std::cout << "Would you like to answer another question? Y/N: ";
+	char again;
+	std::cin >> again;
+	again = toupper(again);
+	if (again == 'Y') {
+		system("clear");
+		return true;
+	}
+	else if (again == 'N') {
+		system("exit");
+	}
+}
 
 
 
